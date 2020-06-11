7343e2ee6dd5166a020fddbf593fba80e2f7157f
Arturo Guardo <arthur0898@gmail.com>
2020-06-11 03:47:45 +0000

    Hello World task 9.0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - use printf typeof
+ * Description: output with printf
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int i = 0;
+char c[65] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+for (i = 0 ; i <= 63 ; i++)
+{
+putchar(c[i]);
+}
+return (1);
+}
