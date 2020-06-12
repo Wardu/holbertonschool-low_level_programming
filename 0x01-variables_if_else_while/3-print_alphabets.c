4f2eb6cfc2a5d72314b4358835b6412c660a24fe
Arturo Guardo <arthur0898@gmail.com>
2020-06-12 01:58:02 +0000

    Variables If Else While task 3.0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry a number to the variable n and print character.
+ *
+ * Description: Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 97 ; c < 123 ; c++)
+	{
+	putchar(c);
+	}
+        for (c = 65 ; c < 91 ; c++)
+        {
+        putchar(c);
+        }
+	putchar('\n');
+
+return (0);
+}
