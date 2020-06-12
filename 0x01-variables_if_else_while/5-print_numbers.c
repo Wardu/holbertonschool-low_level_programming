38091862e4e87e18b7b3ac747e25b9d8314ef856
Arturo Guardo <arthur0898@gmail.com>
2020-06-12 02:32:10 +0000

    Variables If Else While task 4.2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry a number to the variable c and print number.
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+		printf("%i", c);
+	}
+	printf("\n");
+
+return (0);
+}
