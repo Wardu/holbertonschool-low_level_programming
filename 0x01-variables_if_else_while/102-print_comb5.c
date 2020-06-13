85efd6eee087d1cb1a919b66875ffffaad391623
Arturo Guardo <arthur0898@gmail.com>
2020-06-13 04:55:58 +0000

    Variables If Else While task 13.5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
 
 /**
- * main - Entry a number to the variable n and print character.
+ * main - returns every of combo two, two digit numbers without repeating
+ * themselves
  *
- * Description: Prints the alphabet in lowercase.
- *
- * Return: Always 0 (Success)
+ * Return: 0
  */
-
 int main(void)
 {
-int d, c, b, a;
+	int i, j;
+	int a, b, c, d;
 
-for (d = '0' ; d <= '9' ; d++)
-{
-for (c = '0' ; c <= '9' ; c++)
-{
-for (b = d ; b <= '9' ; b++)
-{
-for (a = c + 1 ; a <= '9' ; a++)
-{
-putchar(d);
-putchar(c);
-putchar(' ');
-putchar(b);
-putchar(a);
-if ((d == '9' && c == '8') && (b == '9' && a == '9'))
-{
-break;
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10;
+			d = j % 10;
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+				if (!(a == 9 && b == 8))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-else
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
-putchar('\n');
-return (0);
-}
-
