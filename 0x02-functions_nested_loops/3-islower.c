4f369b16853e4d61f014c9a4437b14dc6f5c0a1c
Arturo Guardo <arthur0898@gmail.com>
2020-06-16 20:40:28 +0000

    Functions Nested Loops task 3.1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,29 +1,15 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
+#include "holberton.h"
 /**
  * _islower - checks for lowercase character
- * @r: The character to check
+ * @c: The character to check
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _islower(int r)
+int _islower(int c)
 {
-if ((r >= 'a') && (r <= 'z'))
+if ((c >= 'a') && (c <= 'z'))
 {
 return (1);
 }
@@ -31,24 +17,4 @@ else
 {
 return (0);
 }
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-int r;
-
-r = _islower('H');
-_putchar(r + '0');
-r = _islower('o');
-_putchar(r + '0');
-r = _islower(108);
-_putchar(r + '0');
-_putchar('\n');
-return (0);
 }
