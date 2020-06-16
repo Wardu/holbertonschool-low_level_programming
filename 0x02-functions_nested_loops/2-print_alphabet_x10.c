72482738e02a85e0d0a095c4727b8225bb1ca132
Arturo Guardo <arthur0898@gmail.com>
2020-06-16 20:10:05 +0000

    Functions Nested Loops task 2.1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,5 @@
 #include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include "holberton.h"
 
 /**
  * main - use funcion _putchar
@@ -19,7 +7,7 @@ return (write(1, &c, 1));
  * Return: On success 0.
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
 int i = 'a', n = 0;
 
@@ -34,5 +22,4 @@ i = 'a';
 _putchar('\n');
 n++;
 }
-return (0);
 }
