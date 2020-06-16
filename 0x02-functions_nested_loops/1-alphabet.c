24f75576c93c251ef23265c133299d738ddf2f3b
Arturo Guardo <arthur0898@gmail.com>
2020-06-16 00:58:56 +0000

    Functions Nested Loops task 1.0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * main - use funcion _putchar
+ * Description: output with return
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+int i = 'a';
+
+while (i <= 'z')
+{
+_putchar(i);
+i++;
+}
+return (0);
+}
