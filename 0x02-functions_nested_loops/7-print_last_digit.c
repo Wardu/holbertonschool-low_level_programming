54e7c94ba445d49612a273a00da3c70621e7ccdc
Arturo Guardo <arthur0898@gmail.com>
2020-06-16 22:30:20 +0000

    Functions Nested Loops task 7.1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,59 +1,29 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include "holberton.h"
 
 /**
  * print_last_digit - check the last digit.
- * @r: The character to check
+ * @c: The character to check
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int print_last_digit(int r)
+int print_last_digit(int c)
 {
-if (r > 0)
+if (c > 0)
 {
-r = r % 10;
-_putchar('0' + r);
+c = c % 10;
+_putchar('0' + c);
 }
-else if (r == 0)
+else if (c == 0)
 {
-_putchar('0' + r);
+_putchar('0' + c);
 }
 else
 {
-r *= -1;
-r = r % 10;
-_putchar('0' + r);
+c *= -1;
+c = c % 10;
+_putchar('0' + c);
 }
-return (r);
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
-
-print_last_digit(98);
-print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
-_putchar('\n');
-return (0);
+return (c);
 }
