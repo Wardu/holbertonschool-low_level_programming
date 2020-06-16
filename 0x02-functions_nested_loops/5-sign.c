8835a173ad761d879903e05be277a4cdbc0065e8
Arturo Guardo <arthur0898@gmail.com>
2020-06-16 21:06:45 +0000

    Functions Nested Loops task 5.1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,19 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+#include "holberton.h"
 
 /**
  * print_sign - check the sign of a number.
- * @r: The character to check
+ * @c: The character to check
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
-int print_sign(int r)
+int print_sign(int c)
 {
-if (r > 0)
+if (c > 0)
 {
 return (1);
 }
-else if (r == 0)
+else if (c == 0)
 {
 return (0);
 }
@@ -35,36 +21,4 @@ else
 {
 return (-1);
 }
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
-
-r = print_sign(98);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(0xff);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-r = print_sign(-1);
-_putchar(',');
-_putchar(' ');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
 }
