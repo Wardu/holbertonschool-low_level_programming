73334373cf4a4a0403750a16a03f2da7f9fefe29
Arturo Guardo <arthur0898@gmail.com>
2020-06-16 20:52:44 +0000

    Functions Nested Loops task 4.1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,33 +1,20 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
  * _isalpha - check for alphabetic character.
- * @r: The character to check
+ * @c: The character to check
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int _isalpha(int r)
+int _isalpha(int c)
 {
-if ((r >= 'a') && (r <= 'z'))
+if ((c >= 'a') && (c <= 'z'))
 {
 return (1);
 }
-else if ((r >= 'A') && (r <= 'Z'))
+else if ((c >= 'A') && (c <= 'Z'))
 {
 return (1);
 }
@@ -35,25 +22,4 @@ else
 {
 return (0);
 }
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-int r;
-
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
 }
