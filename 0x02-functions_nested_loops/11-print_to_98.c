809cd969d56864ef00e965dda7b22052b4bd8ab3
Arturo Guardo <arthur0898@gmail.com>
2020-06-17 04:18:28 +0000

    Functions Nested Loops task 11.1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,24 @@ void print_to_98(int n)
 int i;
 if (n >= 98)
 {
-for (i = n ; i <= 98 ; i++)
+for (i = n ; i >= 98 ; i--)
 {
-printf("%i, ", i);
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
 }
 }
 else
 {
-for (i = n ; i >= 98 ; i--)
+for (i = n ; i <= 98 ; i++)
 {
-printf("%i, ", i);
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
 }
 }
 printf("\n");
