e1674e630c3b5661de662904a5313ccb02abe79b
Arturo Guardo <arthur0898@gmail.com>
2020-06-17 03:47:29 +0000

    Functions Nested Loops task 10.0

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - Write a function that adds two integers and returns the result.
+ * @a: interger
+ * @b: other interger
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
