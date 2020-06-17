40f615983a4451b1c4cc237fbe4772fc9b196388
Arturo Guardo <arthur0898@gmail.com>
2020-06-17 01:59:11 +0000

    Functions Nested Loops task 8.1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,26 +8,17 @@
 
 void jack_bauer(void)
 {
-int h1, h2, m1, m2;
+int h, m;
 
-for (h1 = 0 ; 0 < 3 ; h1++)
+for (h = 0 ; h < 24 ; h++)
+for (m = 0 ; m < 60 ; m++)
 {
-for (h2 = 0 ; 0 <= 9 ; h2++)
-{
-for (m1 = 0 ; 0 < 6 ; m1++)
-{
-for (m2 = 0 ; 0 <= 9 ; m2++)
-{
-_putchar('0' + h1);
-_putchar('0' + h2);
+
+_putchar(h / 10 + '0');
+_putchar(h % 10 + 48);
 _putchar(':');
-_putchar('0' + m1);
-_putchar('0' + m2);
+_putchar(m / 10 + '0');
+_putchar(m % 10 + '0');
 _putchar('\n');
 }
 }
-}
-}
-}
-
-
