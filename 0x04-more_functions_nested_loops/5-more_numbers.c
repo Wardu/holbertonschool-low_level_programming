cdfee46c1a2ca76f7b5efd4fe0ce3bc08b003949
Arturo Guardo <arthur0898@gmail.com>
2020-06-18 20:19:34 +0000

    More Functions, More Nested Loops task 5.0

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10 times, from 0 to 14, followed by a new line.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void more_numbers(void)
+{
+	int i, j, k, m;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+	m = '9';
+		for (j = '0'; j <= '1' ; j++)
+		{
+			for (k = '0' ; k <= m ; k++)
+			{
+				if (j == '1')
+				{
+					_putchar(j);
+				}
+				_putchar(k);
+				if (k == '9')
+				{
+					m = '4';
+				}
+			}
+		}
+	_putchar('\n');
+	}
+}
