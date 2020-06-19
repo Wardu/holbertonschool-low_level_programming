37f5ac866b7e6f155f65bc9484a0eafe9bbf2f40
Arturo Guardo <arthur0898@gmail.com>
2020-06-19 02:38:00 +0000

    More Functions, More Nested Loops task 9.1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,24 @@ int i = 1;
 
 	while (i <= 100)
 	{
-	if ((i % 3 == 0) && (i % 5 == 0))
-	{
-		printf("FizzBuzz ");
-	}
-	else if (i % 3 == 0)
-	{
-		printf("Fizz ");
-	}
-	else if (i % 5 == 0)
-	{
-		printf("Buzz ");
-	}
-	else
-	{
-	printf("%i ", i);
-	}
+		if (((i % 3) || (i % 5)) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
 	i++;
 	}
-printf("\n");
-return (0);
+	printf("\n");
+	return (0);
 }
