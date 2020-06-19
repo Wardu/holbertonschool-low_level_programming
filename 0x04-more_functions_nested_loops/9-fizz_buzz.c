831b39c459cadba2eeeed0a03d8fbf5196a17e70
Arturo Guardo <arthur0898@gmail.com>
2020-06-19 01:58:09 +0000

    More Functions, More Nested Loops task 9.0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - The Fizz-Buzz test.
+ * Description: output with return
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+int i = 1;
+
+	while (i <= 100)
+	{
+	if ((i % 3 == 0) && (i % 5 == 0))
+	{
+		printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else
+	{
+	printf("%i ", i);
+	}
+	i++;
+	}
+printf("\n");
+return (0);
+}
