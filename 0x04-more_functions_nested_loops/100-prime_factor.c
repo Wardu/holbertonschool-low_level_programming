21046bcc94a5e377f5b78fd6d63f6071129c3ea2
Arturo Guardo <arthur0898@gmail.com>
2020-06-19 05:59:55 +0000

    More Functions, More Nested Loops task 11.0

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - program that finds and prints the largest prime factor of the number
+ *
+ * Return: int - 0 is success
+ */
+int main(void)
+{
+	int prime, fac_prime;
+	unsigned long i, j, n = 612852475143;
+
+	for (i = 1 ; i <= n ; i++)
+	{
+		if (n % i == 0)
+		{
+			prime = 0;
+			for (j = 1 ; j <= i ; j++)
+			{
+				if (i % j == 0)
+				prime++;
+			}
+				if (prime == 2)
+				fac_prime = i;
+		}
+	}
+	printf("%d\n", fac_prime);
+	return (0);
+}
+
