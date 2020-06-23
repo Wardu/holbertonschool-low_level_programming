a786edf5c54615795cb994e488fde59739da194d
Wardu <arthur0898@gmail.com>
2020-06-23 15:54:54 -0500

    Pointers, arrays and strings task 1.1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
 	* swap_int - updates the value it points to to 98.
 	* @a: a variable as referencia
 	* @b: a variable as referencia
-	* @n: a variable as referencia
 	* followed by a new line.
 	* Return: none.
 	*/
