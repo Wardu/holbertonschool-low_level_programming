48af0d20b59f975cb3d3321ec1d3b80fdd8e2487
Wardu <arthur0898@gmail.com>
2020-06-23 15:45:02 -0500

    Pointers, arrays and strings task 1.0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+	* swap_int - updates the value it points to to 98.
+	* @a: a variable as referencia
+	* @b: a variable as referencia
+	* @n: a variable as referencia
+	* followed by a new line.
+	* Return: none.
+	*/
+
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+	*a = *b;
+	*b = n;
+}
