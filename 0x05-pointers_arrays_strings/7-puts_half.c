5f744bc075200b7820ed5a91da57cfa52e1e7ccd
Wardu <arthur0898@gmail.com>
2020-06-23 23:44:31 -0500

    Pointers, arrays and strings task 7.5

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,11 +10,11 @@
 void puts_half(char *str)
 {
 	int i, j;
-for (i = 0 ; str[i] != '\0' ; i++)
+for (; str[i] != '\0' ; i++)
 	{
 	}
 j = i / 2;
-	for (; j <= i ; j++)
+	for (; j <= i - 1 ; j++)
 	{
 		_putchar(str[j]);
 	}
