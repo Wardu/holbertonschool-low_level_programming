c6002708b4277e90afac0fef826292f21d5b33e4
Wardu <arthur0898@gmail.com>
2020-06-23 23:28:11 -0500

    Pointers, arrays and strings task 7.0

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+	* puts_half - updates the value it points to to 98.
+	* @str: a variable as referencia
+	* followed by a new line.
+	* Return: none.
+	*/
+
+void puts_half(char *str)
+{
+	int i, j;
+for (; str[i] != '\0' ; i++)
+	{
+	}
+j = i / 2;
+i -= 1;
+	for (; j <= i ; j++)
+	{
+		_putchar(str[j]);
+	}
+_putchar('\n');
+}
