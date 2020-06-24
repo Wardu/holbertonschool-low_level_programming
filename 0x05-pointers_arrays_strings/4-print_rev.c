d131c539dec88caa51f35c7a32a70f7483e7dd5a
Wardu <arthur0898@gmail.com>
2020-06-23 22:07:54 -0500

    Pointers, arrays and strings task 4.0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+	* print_rev - updates the value it points to to 98.
+	* @s: a variable as referencia
+	* followed by a new line.
+	* Return: none.
+	*/
+
+void print_rev(char *s)
+{
+	int i, j;
+for (; s[i] != 0 ; i++)
+	{
+	}
+	for (j = i ; j >= 0 ; j--)
+	{
+		_putchar(s[j]);
+	}
+_putchar('\n');
+}
