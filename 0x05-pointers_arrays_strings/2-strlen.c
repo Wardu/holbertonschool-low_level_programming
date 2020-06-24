7a482ba0b6070f69029bcfb54fa4d40ca8b9edf4
Wardu <arthur0898@gmail.com>
2020-06-23 21:43:18 -0500

    Pointers, arrays and strings task 2.0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+	* swap_int - updates the value it points to to 98.
+	* @s: a variable as referencia
+	* followed by a new line.
+	* Return: none.
+	*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (; s[i] != 0 ; i++)
+	{
+	}
+
+	return (i);
+}
