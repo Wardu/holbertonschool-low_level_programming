2d4a9687aceb03bd2c84ce5c212c38c325394bae
Wardu <arthur0898@gmail.com>
2020-06-25 17:36:51 -0500

    More pointers, arrays and strings 2.1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _strncat - copies a string
+ * _strncpy - copies a string
  * @dest: source to copy string to
 	* @src: source to copy string to
 	* @n: source to copy string to
@@ -13,11 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int j;
 
-for (j = 0 ; dest[j] != '\0' && j < n; j++)
+for (j = 0 ; j < n && dest[j] != '\0'; j++)
 {
 dest[j] = src[j];
 }
-for (; j < n ; j++)
+for ( ; j < n ; j++)
 {
 dest[j] = '\0';
 }
