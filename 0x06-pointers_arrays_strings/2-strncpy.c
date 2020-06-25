815b64609332abf3708aa38d6539c22a403933c5
Wardu <arthur0898@gmail.com>
2020-06-25 17:39:24 -0500

    More pointers, arrays and strings 2.2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
+int j = 0;
 
-for (j = 0 ; j < n && dest[j] != '\0'; j++)
+for (; j < n && dest[j] != '\0' ; j++)
 {
 dest[j] = src[j];
 }
-for ( ; j < n ; j++)
+for (; j < n ; j++)
 {
 dest[j] = '\0';
 }
